29762e4d0e2ec93cb5aaf8d53760198e77909c41
MMamunurRashid <mdmamun.iubat.m@gmail.com>
2024-05-13 17:27:00 +0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+import React, { useState, useEffect } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Hotels from './components/Hotels/Hotels';
+import HotelDetail from './components/HotelDetail/HotelDetail';
+import Booking from './components/Booking/Booking';
+import Navbar from './Shared/Navbar/Navbar';
+
+const App = () => {
+  const [hotels, setHotels] = useState([]);
+  const [selectedHotel, setSelectedHotel] = useState(null);
+  const [selectedRoom, setSelectedRoom] = useState(null);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    // Fetch hotel data
+    async function fetchHotels() {
+      try {
+        const res = await fetch('/hotelData.json');
+        const data = await res.json();
+        setHotels(data.hotels);
+      } catch (error) {
+        console.error('Error fetching hotel data:', error);
+      }
+    }
+
+    fetchHotels();
+  }, []);
+
+  const handleSelectHotel = (hotel) => {
+    setSelectedHotel(hotel);
+    setSelectedRoom(null);
+  };
+
+  const handleSelectRoom = (room) => {
+    setSelectedRoom(room);
+  };
+
+  const handleConfirmBooking = () => {
+    // Implement booking logic
+  };
+
+  const handleSearchChange = (e) => {
+    setSearchQuery(e.target.value);
+  };
+
+  const filteredHotels = hotels.filter(hotel =>
+    hotel.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+    hotel.location.toLowerCase().includes(searchQuery.toLowerCase())
   );
-}
+
+  return (
+    <Router>
+      <div className="container mx-auto px-4 ">
+      <div className='mb-10'>
+      <Navbar/>
+
+      </div>
+        <Routes>
+          <Route path="/" element={<Hotels hotels={filteredHotels} onSelectHotel={handleSelectHotel} />} />
+          <Route path="/hotel-detail/:hotelId" element={<HotelDetail />} />
+          <Route path="/booking/:roomId" element={<Booking />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
 
 export default App;
